48b238bd1bd5130a3edc85d140a9d6432a50704d
Zakariah Reeves-Woods <zakariah.reeveswoods@bsu.edu>
2023-09-05 13:40:02 -0400

    grades plus test with working test import

---
+++ lib/grade_converter.dart
@@ -0,0 +1,15 @@
+class GradeConverter {
+  String convertGrades(double numericGrade) {
+    if (numericGrade >= 90) {
+      return 'A';
+    } else if (numericGrade >= 80) {
+      return 'B';
+    } else if (numericGrade >= 70) {
+      return 'C';
+    } else if (numericGrade >= 60) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
-class GradeConverter {
-  String grades(double numericGrade) {
-    if (numericGrade >= 90) {
-      return 'A';
-    } else if (numericGrade >= 80) {
-      return 'B';
-    } else if (numericGrade >= 70) {
-      return 'C';
-    } else if (numericGrade >= 60) {
-      return 'D';
-    } else {
-      return 'F';
-    }
-  }
+import 'package:test/test.dart';
+
+import 'grade_converter.dart';
+
+void main() {
+  test('Converts 95 to A', () {
+    final gradeConverter = GradeConverter();
+    final letterGrade = gradeConverter.convertGrades(95);
+    expect(letterGrade, 'A');
+  });
+
+  test('Converts 85 to B', () {
+    final gradeConverter = GradeConverter();
+    final letterGrade = gradeConverter.convertGrades(85);
+    expect(letterGrade, 'B');
+  });
+
+  test('Converts 75 to C', () {
+    final gradeConverter = GradeConverter();
+    final letterGrade = gradeConverter.convertGrades(75);
+    expect(letterGrade, 'C');
+  });
+
+  test('Converts 65 to D', () {
+    final gradeConverter = GradeConverter();
+    final letterGrade = gradeConverter.convertGrades(65);
+    expect(letterGrade, 'D');
+  });
+
+  test('Converts 55 to F', () {
+    final gradeConverter = GradeConverter();
+    final letterGrade = gradeConverter.convertGrades(55);
+    expect(letterGrade, 'F');
+  });
 }
